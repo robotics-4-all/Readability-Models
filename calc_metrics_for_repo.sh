30f9b213b335283f604054d262d139acc960ce14
anestv <anestv@users.noreply.github.com>
2021-06-03 01:27:13 +0300

    Corrected some mistakes, moved numstat.txt

--- calc_metrics_for_repo.sh
+++ calc_metrics_for_repo.sh
@@ -1,24 +1,27 @@
 
 
+# maybe these paths & consts should be defined outside, as "env"
+# Also, ensure that the directories exist
+SCRIPTS_DIR=$(pwd) #TODO
+METRICS_DIR="$(pwd)/metrics" #TODO
+KEYWORDS="readability readable" # TODO $(cat keywords.txt)
 
-SCRIPTS_DIR=..... #TODO
-METRICS_DIR=..... #TODO
 
 # TODO cd to project's git directory. First clone it.
 
 
 # find radability commits
 
-for keyword in $keywords ; do
+for keyword in $KEYWORDS ; do
 
-	git log --all --grep="$keyword" --pretty=oneline >> readability_commits_repeated.txt
+	git log --all --grep="$keyword" --pretty=oneline --regexp-ignore-case >> readability_commits_repeated.txt
 	# all means search commits from all branches
 done
 
 sort -u readability_commits_repeated.txt > readability_commits_unique.txt
 # keep only uniques
 
-cut --fields=1 --delimiter=' ' readability_commits_unique.txt
+#cut --fields=1 --delimiter=' ' readability_commits_unique.txt
 # keep only first column (commit hash)
 
 echo "Found $(wc --lines < readability_commits_unique.txt) readability commits!"
@@ -28,27 +31,27 @@ echo "Found $(wc --lines < readability_commits_unique.txt) readability commits!"
 #-----------------
 #for each commit: (maybe select x eg.400 readability commits, and 4*x random other commits for comparison)
 
-for commit in $(cat readability_commits_unique.txt nonread_commits.txt)
+for commit in $(cut -f1 --delimiter=' ' readability_commits_unique.txt) $(cat nonread_commits.txt); do
 
-	short_hash=$(echo $commit | cut -c10)
+	short_hash=$(echo $commit | cut -c1-10)
 	
 	# setup CSV files and their headers
-	SCRIPTS_DIR/calc_metrics_file.py --setup > $RESULTS_DIR/${short_hash}_after.csv
-	cp $RESULTS_DIR/${short_hash}_after.csv $RESULTS_DIR/${short_hash}_befor.csv
+	$SCRIPTS_DIR/calc_metrics_file.py --setup > $METRICS_DIR/${short_hash}_after.csv
+	cp $METRICS_DIR/${short_hash}_after.csv $METRICS_DIR/${short_hash}_befor.csv
 	
 	# checkout to after commit
 	git checkout $commit
 	
 	# find list of files changed and num of lines changed
-	git diff --numstat HEAD^ > numstat.txt
+	git diff --numstat HEAD^ > $METRICS_DIR/${short_hash}_numstat.txt
 	
-	files_changed=$(cut -f3 numstat.txt) # the 3rd column has the filenames
+	files_changed=$(cut -f3 $METRICS_DIR/${short_hash}_numstat.txt) # the 3rd column has the filenames
 	
 	for file in $files_changed ; do
 	
 		# Calculate various metrics for file after commit. Store results
 		
-		$SCRIPTS_DIR/calc_metrics_file.py $file >> $RESULTS_DIR/${short_hash}_after.csv
+		$SCRIPTS_DIR/calc_metrics_file.py $file >> $METRICS_DIR/${short_hash}_after.csv
 		
 		# one line per file. Filename can be the first field
 	done
@@ -59,7 +62,7 @@ for commit in $(cat readability_commits_unique.txt nonread_commits.txt)
 	for file in $files_changed ; do
 		# Calculate various metrics for file before commit. Store results
 		
-		$SCRIPTS_DIR/calc_metrics_file.py $file >> $RESULTS_DIR/${short_hash}_befor.csv
+		$SCRIPTS_DIR/calc_metrics_file.py $file >> $METRICS_DIR/${short_hash}_befor.csv
 	done
 	
 	
