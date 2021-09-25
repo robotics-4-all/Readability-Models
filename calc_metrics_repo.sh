7524f7a5413d15638e9ab992b70cf010f6863b50
anestv <anestv@users.noreply.github.com>
2021-09-25 14:08:45 +0300

    Run SourceMeter in calc_metrics_repo

--- calc_metrics_repo.sh
+++ calc_metrics_repo.sh
@@ -44,8 +44,64 @@ sort nonread_commits_unchecked.txt | comm -13 readability_commits_unique.txt - >
 #TODO rm readability_commits_repeated.txt nonread_commits_unchecked.txt
 
 
+function runSMA () {
+	$SCRIPTS_DIR/SourceMeterJava -resultsDir=/tmp/SMAresults -projectName=$1 -projectBaseDir=. \
+		-runFB=false -runPMD=false -runAndroidHunter=false -runMetricHunter=false \
+		-runVulnerabilityHunter=false -runFaultHunter=false -runRTEHunter=false \
+		-runDCF=false -runMET=true $files_changed
+		#TODO check if the command works
+	
+	# We just want it to calc metrics: runMET
+	# projectBaseDir is the current, the base git dir.
+	#TODO does this work? -projectBaseDir=.
+}
+
 #-----------------
 #for each commit: (from x eg.50 readability commits, and 2*x random other commits for comparison)
+
+
+sudo update-java-alternatives -s java-1.11.0-openjdk-amd64
+# SourceMeter needs Java v1.11
+
+for commit in $(cat readability_commits_unique.txt nonread_commits.txt); do
+
+	short_hash=$(echo $commit | cut -c1-10)
+	
+	git checkout $commit
+	
+	# find list of files changed and num of lines changed
+	git diff --numstat --diff-filter=M HEAD^ > $METRICS_DIR/${short_hash}_numstat.txt
+	
+	# We only want files which exist before and after the commit. So no added/deleted.
+	# The option --diff-filter=M keeps only modified files.
+	
+	files_changed=$(cut -f3 $METRICS_DIR/${short_hash}_numstat.txt | grep '\.java$') # the 3rd column has the filenames
+	# only include .java files (maybe shouldn't? TODO)
+	#sed -E 's/.*\t.*\t(.*\.java)$/$1/;t;d' $METRICS_DIR/${short_hash}_numstat.txt
+	#cut -f3 $METRICS_DIR/${short_hash}_numstat.txt | grep '\.java$'
+	#grep --perl-regexp --only-matching '[^\t]+\.java$' $METRICS_DIR/${short_hash}_numstat.txt
+	
+	runSMA after & # run SourceMeter with minimal options
+	# WITH " &" so that they run in parallel
+	
+	git checkout HEAD^ # to before commit
+	
+	runSMA befor
+	
+	wait # for the other run of SMA to finish
+	
+	# keep only -Class.csv. * is for the timestamp
+	mv /tmp/SMAresults/after/java/*/after-Class.csv $METRICS_DIR/${short_hash}_sma_after.csv
+	mv /tmp/SMAresults/befor/java/*/befor-Class.csv $METRICS_DIR/${short_hash}_sma_befor.csv
+	
+	rm -rf /tmp/SMAresults/
+	
+done
+
+
+sudo update-java-alternatives -s java-1.14.0-openjdk-amd64
+# The other jars need Java v1.14
+
 
 for commit in $(cat readability_commits_unique.txt nonread_commits.txt); do
 
@@ -58,20 +114,15 @@ for commit in $(cat readability_commits_unique.txt nonread_commits.txt); do
 	# checkout to after commit
 	git checkout $commit
 	
-	# find list of files changed and num of lines changed
-	git diff --numstat --diff-filter=MR HEAD^ > $METRICS_DIR/${short_hash}_numstat.txt
-	
-	# We only want filees which exist before and after the commit. So no added/deleted.
-	# The option --diff-filter=MR keeps only modified and renamed files.
-	
-	# TODO what if a line is renamed, like:
-	# 17      2       calc_metrics_for_repo.sh => calc_metrics_repo.sh
-	
+	# find list of files changed and num of lines changed. We did 'git numstat' above
 	files_changed=$(cut -f3 $METRICS_DIR/${short_hash}_numstat.txt | grep '\.java$') # the 3rd column has the filenames
 	# only include .java files (maybe shouldn't? TODO)
 	#sed -E 's/.*\t.*\t(.*\.java)$/$1/;t;d' $METRICS_DIR/${short_hash}_numstat.txt
 	#cut -f3 $METRICS_DIR/${short_hash}_numstat.txt | grep '\.java$'
 	#grep --perl-regexp --only-matching '[^\t]+\.java$' $METRICS_DIR/${short_hash}_numstat.txt
+	
+	# use the result of SourceMeter for this commit
+	cp $METRICS_DIR/${short_hash}_sma_after.csv $METRICS_DIR/curr_sma_result.csv
 	
 	for file in $files_changed ; do
 	
@@ -85,6 +136,9 @@ for commit in $(cat readability_commits_unique.txt nonread_commits.txt); do
 	
 	git checkout HEAD^ # to before commit
 	
+	# use the result of SourceMeter for (before) this commit
+	cp $METRICS_DIR/${short_hash}_sma_befor.csv $METRICS_DIR/curr_sma_result.csv
+	
 	for file in $files_changed ; do
 		# Calculate various metrics for file before commit. Store results
 		
@@ -92,15 +146,5 @@ for commit in $(cat readability_commits_unique.txt nonread_commits.txt); do
 	done
 	
 	
-	#TODO in a python script:
-	for each metric:
-		find out whether it:
-		  - showed no significant change in all files
-		  - increased or unchanged in all files (by how much?)
-		  - decreased or unchanged in all files (how much?)
-		  - increased in some files, decreased in others. (Can we determine which is greater? Increase or decrease? How to choose? Num of lines changed per file? Total SLOC per file? For now maybe just categorise in these 4, and don''t determine which is greater)
-	
-	done
-
 done
 
